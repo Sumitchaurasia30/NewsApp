36a1da47ba6ee24f78937e5b2f80f86a8dca3fa6
Sumitchaurasia30 <sumitmchaurasia@gmail.com>
2025-05-15 12:18:55 +0530

    vercel file added

--- public/api/news.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(`https://newsapi.org/v2/everything?q=${q}&pageSize=24&page=1&sortBy=publishedAt&language=${searchParams.get("language") ?? "hi"}&apiKey=0919f10e7ab541d4873bf3b6cd6b5bbb`)
-  const data = await response.json();
-  res.status(200).json(data);
-}
